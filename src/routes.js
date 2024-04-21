754384a83d4455a31129d1c7296ba763a945e70c
pxlxth <poleth.tordoya@gmail.com>
2024-04-20 20:06:41 -0400

    update

--- src/routes.js
+++ src/routes.js
@@ -6,18 +6,28 @@ const BuscarAulas = React.lazy(() => import('./views/pages/buscar-aulas/BuscarAu
 const CrearReservas = React.lazy(() => import('./views/pages/crear-reservas/CrearReservas'))
 const Solicitudes = React.lazy(() => import('./views/pages/solicitudes/Solicitudes'))
 const Administrar = React.lazy(() => import('./views/pages/administrar/Administrar'))
+const Roles = React.lazy(() => import('./views/pages/usuarios/roles/Role'))
+const CrearRoles = React.lazy(() => import('./views/pages/usuarios/roles/RoleForm'))
+const EditarRole = React.lazy(() => import('./views/pages/usuarios/roles/RoleForm'))
 const Calendario = React.lazy(() => import('./components/calendario/Calendario'))
-const Feriados = React.lazy(() => import('./views/pages/feriados/Feriados'))
+// const Feriados = React.lazy(() => import('./views/pages/module-feriados/feriados/Feriados'))
 const AgregarMateria = React.lazy(() => import('./views/pages/agregar-materia/AgregarMateria'))
-const registrarMateria = React.lazy(() => import('./views/pages/registrar-materias/registrarMateria'))
+const registrarMateria = React.lazy(
+  () => import('./views/pages/registrar-materias/registrarMateria'),
+)
 const AgregarUnidad = React.lazy(() => import('./views/pages/agregar-unidad/AgregarUnidad'))
 const registrarUnidad = React.lazy(() => import('./views/pages/registrar-unidad/registrarUnidad'))
 const AgregarDepartamento = React.lazy(() => import('./views/pages/agregar-departamento/AgregarDepartamento'))
 const RegistrarDepartamento = React.lazy(() => import('./views/pages/registrar-departamentos/registrarDepartamento'))
 
-
-// const CrearFeriados = React.lazy(() => import('./views/pages/crear-feriados/CreateFeriados'))
-import CrearFeriados from './views/pages/crear-feriados/CreateFeriados'
+const ListUser = React.lazy(() => import('./views/pages/usuarios/users/List.jsx'))
+const FormUser = React.lazy(() => import('./views/pages/usuarios/users/Form.jsx'))
+import Feriados from './views/pages/module-feriados/feriados/Feriados'
+import DetailsFeriado from './views/pages/module-feriados/details-feriado/DetailsFeriado.js'
+const CrearFeriados = React.lazy(
+  () => import('./views/pages/module-feriados/crear-feriados/CreateFeriados'),
+)
+// import CrearFeriados from './views/pages/crear-feriados/CreateFeriados'
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
@@ -25,11 +35,28 @@ const routes = [
   { path: '/reservas/crear-reservas', name: 'Crear reservas', element: CrearReservas },
   { path: '/configurar/solicitudes', name: 'Solicitudes', element: Solicitudes },
   { path: '/configurar/administrar', name: 'Administrar', element: Administrar },
+  { path: '/configurar/roles', name: 'Roles', element: Roles },
+  { path: '/configurar/crear-roles', name: 'Crear Role', element: CrearRoles },
+  { path: '/configurar/editar-role/:id', name: 'Editar Role', element: EditarRole },
   { path: '/administracion/calendario', name: 'Calendario', element: Calendario },
   { path: '/administracion/feriados', name: 'Feriados', element: Feriados },
   { path: '/administracion/crear-feriados', name: 'Crear Feriado', element: CrearFeriados },
   { path: '/administracion/agregar-materia', name: 'Agregar Materia', element: AgregarMateria },
-  { path: '/administracion/registrar-materias', name: 'Agregar Materia', element: registrarMateria },
+  {
+    path: '/administracion/feriados/:id/details',
+    name: 'Detalles Feriado',
+    element: DetailsFeriado,
+  },
+  {
+    path: '/administracion/registrar-materias',
+    name: 'Agregar Materia',
+    element: registrarMateria,
+  },
+  //USUARIOS
+  { path: '/users', name: 'Usuarios', element: ListUser },
+  { path: '/users/create', name: 'Crear Usuario', element: FormUser },
+  { path: '/users/:id/edit', name: 'Editar Usuario', element: FormUser },
+  //unidades
   { path: '/administracion/agregar-unidad', name: 'Unidades', element: AgregarUnidad },
   { path: '/administracion/registrar-unidad', name: 'Agregar unidad', element: registrarUnidad },
   { path: '/administracion/Departamentos', name: 'Departamentos', element: AgregarDepartamento },
--- src/store.js
+++ src/store.js
@@ -1,39 +1,20 @@
 import { legacy_createStore as createStore } from 'redux'
-import { AGREGAR_FERIADO, EDITAR_FERIADO, LISTAR_FERIADO, BORRAR_FERIADO, AGREGAR_MATERIA,LISTAR_MATERIA, AGREGAR_UNIDAD, LISTAR_UNIDAD} from './actions'
+import {
+  AGREGAR_FERIADO,
+  EDITAR_FERIADO,
+  LISTAR_FERIADO,
+  BORRAR_FERIADO,
+  AGREGAR_MATERIA,
+  LISTAR_MATERIA,
+  AGREGAR_UNIDAD, 
+  LISTAR_UNIDAD
+} from './actions'
+import { getFeriados } from './views/pages/module-feriados/service'
 
 export const initialState = {
   sidebarShow: true,
   theme: 'light',
-  feriados: [
-    {
-      id: 1,
-      fecha: '13/4/2024',
-      codigo: 'COD-001',
-      description: 'Lorem Ipsum has been the industrys',
-    },
-    {
-      id: 2,
-      fecha: '13/4/2024',
-      codigo: 'COD-002',
-      description: 'Lorem Ipsum has been the industrys',
-    },
-    {
-      id: 3,
-      fecha: '13/4/2024',
-      codigo: 'COD-003',
-      description: 'Lorem Ipsum has been the industrys',
-    },
-  ],
-
-  materias: [
-    {
-      id: 1,
-      departamento: "Sistemas",
-      materia:"Elementos",
-      codigo: 20100013,
-      
-    }
-  ],
+  feriados: [],
   unidades: [{
     id: 1,
     nombreUnidades: "Edificio academico II",
@@ -50,6 +31,7 @@ export const initialState = {
     horaCierreUnidades: "14:15",
     departamento: "Ninguno"
   },
+ 
   {
     id: 1,
     nombreUnidades: "Edificio academico II",
@@ -73,80 +55,7 @@ export const initialState = {
     horaAperturaUnidades:"6:45",
     horaCierreUnidades: "14:15",
     departamento: "Ninguno"
-  },
-  {
-    id: 1,
-    nombreUnidades: "Edificio academico II",
-    ubicacionUnidades: "ala este",
-    horaAperturaUnidades:"6:45",
-    horaCierreUnidades: "14:15",
-    departamento: "Ninguno"
-  },
-  {
-    id: 1,
-    nombreUnidades: "Edificio academico II",
-    ubicacionUnidades: "ala este",
-    horaAperturaUnidades:"6:45",
-    horaCierreUnidades: "14:15",
-    departamento: "Ninguno"
-  },
-  {
-    id: 1,
-    nombreUnidades: "Edificio academico II",
-    ubicacionUnidades: "ala este",
-    horaAperturaUnidades:"6:45",
-    horaCierreUnidades: "14:15",
-    departamento: "Ninguno"
-  },
-  {
-    id: 1,
-    nombreUnidades: "Edificio academico II",
-    ubicacionUnidades: "ala este",
-    horaAperturaUnidades:"6:45",
-    horaCierreUnidades: "14:15",
-    departamento: "Ninguno"
-  },
-  {
-    id: 1,
-    nombreUnidades: "Edificio academico II",
-    ubicacionUnidades: "ala este",
-    horaAperturaUnidades:"6:45",
-    horaCierreUnidades: "14:15",
-    departamento: "Ninguno"
-  },
-  {
-    id: 1,
-    nombreUnidades: "Edificio academico II",
-    ubicacionUnidades: "ala este",
-    horaAperturaUnidades:"6:45",
-    horaCierreUnidades: "14:15",
-    departamento: "Ninguno"
-  },
-  {
-    id: 1,
-    nombreUnidades: "Edificio academico II",
-    ubicacionUnidades: "ala este",
-    horaAperturaUnidades:"6:45",
-    horaCierreUnidades: "14:15",
-    departamento: "Ninguno"
-  },
-  {
-    id: 1,
-    nombreUnidades: "Edificio academico II",
-    ubicacionUnidades: "ala este",
-    horaAperturaUnidades:"6:45",
-    horaCierreUnidades: "14:15",
-    departamento: "Ninguno"
-  },
-  {
-    id: 1,
-    nombreUnidades: "Edificio academico II",
-    ubicacionUnidades: "ala este",
-    horaAperturaUnidades:"6:45",
-    horaCierreUnidades: "14:15",
-    departamento: "Ninguno"
-  },
-]
+  }]
 }
 
 export const reducer = (state = initialState, { type, ...rest }) => {
@@ -158,13 +67,14 @@ export const reducer = (state = initialState, { type, ...rest }) => {
         ...state,
         feriados: [...state.feriados, rest.payload],
       }
-    case AGREGAR_MATERIA:{
-      console.log(" agregarrr")
+    case AGREGAR_MATERIA: {
+      console.log(' agregarrr')
       return {
         ...state,
         materias: [...state.materias, rest.payload],
       }
-    }case AGREGAR_UNIDAD:{
+    }
+    case AGREGAR_UNIDAD:{
       console.log(" agregarrr")
       return {
         ...state,
@@ -181,18 +91,22 @@ export const reducer = (state = initialState, { type, ...rest }) => {
         ...state,
       }
     }
-    case LISTAR_FERIADO:
+    case LISTAR_FERIADO: {
+      getFeriados().then((response) => {
+        console.log(response)
+      })
       return state
-
-      case LISTAR_MATERIA:
-        return state
-      case LISTAR_UNIDAD:
-        return state
+    }
     case BORRAR_FERIADO:
       return {
         ...state,
         feriados: [...state.postList.filter((feriado) => feriado.id !== action.id)],
       }
+
+      case LISTAR_UNIDAD:
+        return state
+   
+       
     default:
       return state
   }

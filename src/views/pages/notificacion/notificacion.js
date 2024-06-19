c8ac0bdc7cfec4c2cc09398e4762b7adfa047462
DanitSandoval <danitzasanvilla@gmail.com>
2024-06-19 02:17:19 -0400

    correcciones notificaciones

--- src/views/pages/notificacion/notificacion.js
+++ src/views/pages/notificacion/notificacion.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import {
   CCard,
   CCardBody,
@@ -14,65 +14,27 @@ import {
   CTableRow,
   CContainer,
   CButton,
-} from '@coreui/react'
+} from "@coreui/react"
 
 const Notificacion = () => {
   const [notificaciones, setNotificaciones] = useState([])
-  const [solicitudes, setSolicitudes] = useState({})
-  const [idUsuario, setIdUsuario] = useState('')
+  const [idUsuario, setIdUsuario] = useState("")
   const [userData, setUserData] = useState(null)
-  const [aulas, setAulas] = useState({}) // Nuevo estado para almacenar los nombres de las aulas
 
   const navigate = useNavigate()
 
   const handleVerDetalles = (id) => {
-    navigate('/docente/detallesNotificacion', { state: { id } })
+    navigate("/docente/detallesNotificacion", { state: { id } })
   }
 
   const fetchNotificaciones = async () => {
     const response = await fetch(`${process.env.PATH_API}/api/notificaciones`) // Ajusta la URL de tu API
     const data = await response.json()
-
-    const solicitudesData = {}
-    const aulasData = {}
-    const filteredNotificaciones = []
-
-    for (const notificacion of data) {
-      if (!solicitudesData[notificacion.id_solicitud]) {
-        const solicitud = await fetchSolicitud(notificacion.id_solicitud)
-        solicitudesData[notificacion.id_solicitud] = solicitud
-
-        if (solicitud.id_user === idUsuario) {
-          filteredNotificaciones.push(notificacion)
-
-          const aulaId = solicitud.id_aula
-          if (!aulasData[aulaId]) {
-            const aula = await fetchAula(aulaId)
-            aulasData[aulaId] = aula.nombreAulas
-          }
-        }
-      }
-    }
-
-    setNotificaciones(filteredNotificaciones)
-    setSolicitudes(solicitudesData)
-    setAulas(aulasData) // Actualizar el estado de aulas
-  }
-
-  const fetchSolicitud = async (idSolicitud) => {
-    const response = await fetch(`${process.env.PATH_API}/api/solicitud/${idSolicitud}`)
-    const data = await response.json()
-    return data.data
-  }
-
-  const fetchAula = async (idAula) => {
-    const response = await fetch(`${process.env.PATH_API}/api/aula/${idAula}`)
-    const data = await response.json()
-    return data.data
+    setNotificaciones(data)
   }
 
   useEffect(() => {
-    const userDataFromStorage = localStorage.getItem('user_data')
+    const userDataFromStorage = localStorage.getItem("user_data")
     if (userDataFromStorage) {
       const userData = JSON.parse(userDataFromStorage)
       setUserData(userData)
@@ -90,7 +52,7 @@ const Notificacion = () => {
     <CContainer className="px-4">
       <CRow>
         <CCol>
-          <h1 style={{ fontSize: '1.7rem' }}>Lista de Notificaciones</h1>
+          <h1 style={{ fontSize: "1.7rem" }}>Lista de Notificaciones</h1>
         </CCol>
       </CRow>
       <CRow>
@@ -110,8 +72,12 @@ const Notificacion = () => {
             {notificaciones.map((notificacion) => (
               <CTableRow key={notificacion.id}>
                 <CTableDataCell>
-                  Solicitud para el Ambiente{' '}
-                  {aulas[solicitudes[notificacion.id_solicitud]?.id_aula]}
+                  Solicitud para el Ambiente{" "}
+                  {notificacion.solicitud &&
+                  notificacion.solicitud.aulas &&
+                  notificacion.solicitud.aulas.nombreAulas
+                    ? notificacion.solicitud.aulas.nombreAulas
+                    : ""}
                 </CTableDataCell>
                 <CTableDataCell>
                   <CButton color="primary" onClick={() => handleVerDetalles(notificacion.id)}>
